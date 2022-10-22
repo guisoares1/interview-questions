98f2d75ebf8f2721c198d3c9cf5c1691408bc444
Guilherme Soares Correa <55603259+guisoares1@users.noreply.github.com>
2022-10-22 15:33:09 -0300

    Create Number of Islands.cpp

---
+++ questions/Graph/Number of Islands.cpp
@@ -0,0 +1,31 @@
+class Solution {
+int n =0;
+int m =0;
+    
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        m = grid.size(),
+        n = grid[0].size();
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+private:
+    void dfs(vector<vector<char>>& grid, int i, int j) {
+        if (i < 0 || i == m || j < 0 || j == n || grid[i][j] == '0') {
+            return;
+        }
+        grid[i][j] = '0';
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
+    }
+};
