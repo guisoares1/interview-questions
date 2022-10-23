5c591cba3168f3f86464af292d2a900e98238767
Guilherme Soares Correa <55603259+guisoares1@users.noreply.github.com>
2022-10-23 17:59:25 -0300

    Create Items in containers.cpp

---
+++ questions/Arrays/Items in containers.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+
+/*
+ * Complete the 'numberOfItems' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. INTEGER_ARRAY startIndices
+ *  3. INTEGER_ARRAY endIndices
+ */
+
+vector<int> numberOfItems(string s, vector<int> startIndices, vector<int> endIndices) {
+    vector<int> compartmentIndex, ret;
+    for(int cont = 0; cont<s.size(); cont++){
+        if(s[cont]=='|')
+            compartmentIndex.push_back(cont);
+    }
+    // [2 , 5 , 7, 10]
+    // (3, 5) -> 1, 1
+    for(int cont = 0; cont<startIndices.size(); cont++){
+        int start = lower_bound(compartmentIndex.begin(), compartmentIndex.end(), startIndices[cont]-1) - compartmentIndex.begin();
+        int end   = upper_bound(compartmentIndex.begin(), compartmentIndex.end(), endIndices[cont]-1) - compartmentIndex.begin() - 1;
+        
+        if(start<end)
+        {
+            int items = (compartmentIndex[end] - compartmentIndex[start]) - (end-start);
+            ret.push_back(items);
+        }
+        else
+            ret.push_back(0);
+    }
+    return ret;
+}
+
