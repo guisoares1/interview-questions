604cc36203cadd17a8370bf3366e7a859456e721
Guilherme Soares Correa <55603259+guisoares1@users.noreply.github.com>
2022-11-04 17:53:38 -0300

    coments

--- questions/Arrays/Best Time to Buy and Sell Stock.cpp
+++ questions/Arrays/Best Time to Buy and Sell Stock.cpp
@@ -5,12 +5,14 @@ public:
        int atual = 0;
        int conta = 0;
        
+        // array que percorre vetor de preços
         for(int data: prices){
            
             atual = data;
+            // 1 - Verifico se estou na primeira verificação, caso contrário verifico também se o valor atual - anterior é maior que o valor que tenho guardado até o momento
             if(anterior!=-1 && (atual - anterior)>conta)
                 conta = atual - anterior;
-            
+            // caso seja a primeira verificação, o anterior passa a ser o valor atual, caso contrário verifico se ele é maior que o valor atual do vetor
             if(anterior==-1)
                 anterior = data;
             else if(anterior>data)
--- questions/Arrays/Items in containers.cpp
+++ questions/Arrays/Items in containers.cpp
@@ -18,14 +18,16 @@ string rtrim(const string &);
 
 vector<int> numberOfItems(string s, vector<int> startIndices, vector<int> endIndices) {
     vector<int> compartmentIndex, ret;
+    // set the positions of containers
     for(int cont = 0; cont<s.size(); cont++){
         if(s[cont]=='|')
             compartmentIndex.push_back(cont);
     }
-    // [2 , 5 , 7, 10]
-    // (5, 7) -> 1, 2
+
     for(int cont = 0; cont<startIndices.size(); cont++){
+        // look for the first index in compartmentIndex that is less than or equal to startIndices. I took -1 because the index of array start with 1
         int start = lower_bound(compartmentIndex.begin(), compartmentIndex.end(), startIndices[cont]-1) - compartmentIndex.begin();
+        // look for the first index in compartmentIndex that is more than or equal to endIndices. I took -1 because the index of array start with 1
         int end   = upper_bound(compartmentIndex.begin(), compartmentIndex.end(), endIndices[cont]-1) - compartmentIndex.begin() - 1;
         
         if(start<end)
@@ -33,6 +35,14 @@ vector<int> numberOfItems(string s, vector<int> startIndices, vector<int> endInd
             // (end-start) -> tratment of the case than dont have any items betwen '|'
             int items = (compartmentIndex[end] - compartmentIndex[start]) - (end-start);
             ret.push_back(items);
+
+            // exemplo 
+            // array of compartmentIndex = [2,5]
+            // startIndices = [2]
+            // endIndices = [5]
+            // start = 0
+            // end = 1
+            // intems = 5 - 2 - (1-0) -> 2
         }
         else
             ret.push_back(0);
